cd7aaca3bf14dc1de9361c9c978e8570834d2865
euge1982 <148292131+euge1982@users.noreply.github.com>
2025-10-06 20:02:49 -0300

    Ejercicio 4 sin estilos

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 This template should help get you started developing with Vue 3 in Vite.
 
-## EJERCICIO N°
+## EJERCICIO N° 4
+Objetivo: Practicar v-if / v-show, animaciones CSS/Tailwind y directivas de Vue. 
+
+* App.vue tiene un botón "Abrir menú". Al hacer clic, aparece un menú lateral (div) usando v-if o v-show. 
+
+* El menú debe entrar con una animación sencilla (por ejemplo, usando Tailwind: transition transform duration-300 translate-x-0 cuando está visible y -translate-x-full cuando está oculto). 
+
+* El menú contiene una lista con enlaces simulados (ul con 3 <li>).
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const mostrar = ref(false);
 </script>
 
 <template>
   <div>
-    <h1>EJERCICIO N°!</h1>
+    <h1>EJERCICIO N° 4</h1>
+
+    <button @click="if (mostrar) mostrar=false; else mostrar=true">Abrir menu</button>
+    <div v-show="mostrar" :class="mostrar ? 'transition transform duration-300 translate-x-0' : 'transition transform duration-300 -translate-x-full'">
+      <ul>
+        <li><a href="#">Enlace 1</a></li>
+        <li><a href="#">Enlace 2</a></li>
+        <li><a href="#">Enlace 3</a></li>
+      </ul>
+
+    </div>
   </div>
 
 
